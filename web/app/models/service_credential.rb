ff68f1a2e544f3cad4e981ff2c7ecb10735f8911
Piyush Ranjan <piyush.ranjan@planetspark.in>
2025-04-07 11:48:43 +0530

    service cred fix

--- web/app/models/service_credential.rb
+++ web/app/models/service_credential.rb
@@ -1,3 +1,9 @@
 class ServiceCredential < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: 
+  
+  def self.method_missing(m, *args, &block)
+    service_credential = ServiceCredential.find_by(name: m.to_s)
+    return OpenStruct.new() if service_credential.blank?
+    service_credential  # Return the found credential
+  end
 end
